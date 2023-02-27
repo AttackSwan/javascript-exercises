d6b9f791bee407fdbb204e5d9c43c3116018b75c
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-02-27 15:57:58 +1100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let returnString = '';
+    
+    //Check for negative repeats
+    if (num < 0){
+        returnString = "ERROR"
+    }
+    else{
+        for(let i=0; i<num; i++){
+            returnString += string;
+        }
+    }
+    return returnString;
 };
 
 // Do not edit below this line
