7c692b59dbc342aa77c74146fd9d0bf424ed1b4d
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-02-28 11:28:05 +1100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...argRest) {
+    let newArray = [];
 
-};
+    //For each element in the input array,
+    //Check to see if it's in argRest (to be removed)
+    //If it isn't, push it into newArray (to return)
+
+    inputArray.forEach(checkArgs);
+        
+    function checkArgs(element){
+        if (!argRest.includes(element)){
+            newArray.push(element);
+        }
+    }
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
