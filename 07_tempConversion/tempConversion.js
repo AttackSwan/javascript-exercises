45d40654bdf4e77b6c27f7dae1ad6c8f72e0d689
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-02-28 15:00:41 +1100

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celcius = (temperature - 32) * (5/9);
+  celcius = Math.round(celcius * 10)/10;
+  return celcius;
+
+  //can be compounded to:
+  //return Math.round((temperature - 32)* (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = temperature * (9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  //can be compounded to:
+  //return Math.round((temperature * (9/5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
