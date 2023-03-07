7884de45514309c9313154ed49eadae2d2fb95a9
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-03-07 18:11:07 +1100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    return obj.reduce((oldestPerson, currentPerson) => {
+        
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    
+        return oldestAge < currentAge ? currentPerson : oldestPerson;  
+    });
 };
 
-// Do not edit below this line
+const getAge = function(birth, death) {
+    if(!death) { 
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+    // Initial approach using sort
+    // const year = new Date().getFullYear();
+        
+    // const ordered = obj.sort((a, b) => {
+    //     if(!b.yearOfDeath){ b.yearOfDeath = year; }
+    //     const previous = a.yearOfDeath - a.yearOfBirth;
+    //     const next = b.yearOfDeath - b.yearOfBirth;
+    //     return previous > next ? -1 : 1;
+    // });
+    // return ordered.shift();
+
+    // Do not edit below this line
 module.exports = findTheOldest;
