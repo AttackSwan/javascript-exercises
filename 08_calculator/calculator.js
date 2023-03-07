bfd47e3cf449926efb78625ac3c174798242d78f
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-03-07 16:29:45 +1100

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.length ? array.reduce((total, current) => total * current) : 0;
+}
 
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	let sum = 1;
+  if (sum > 0){
+    for(i=a; i>0; i--){
+      sum *= i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const forward = input.toLowerCase().replace(/[\W]/g, "");
+    let reverse = forward.split("").reverse().join("");
 
+    return forward === reverse;
 };
 
 // Do not edit below this line
